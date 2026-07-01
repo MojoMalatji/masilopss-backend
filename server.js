f6dd53753d40f0eff8d6d907ec52fd131cd5693c
khomotjo <mojomalatji@gmail.com>
2026-07-01 17:42:33 +0200

    fix firebase key removal for deployment

--- server.js
+++ server.js
@@ -23,13 +23,28 @@ app.use((req, res, next) => {
 
 // ---------------- FIREBASE ----------------
 console.log("1. Server started");
-
-const serviceAccount = require("./firebaseKey.json");
+ s
 
 const { initializeApp, cert, getApps } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
 console.log("2. Firebase key loaded");
+require("dotenv").config();
+
+const { initializeApp, cert, getApps } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
+
+const db = getFirestore();
 
 if (!getApps().length) {
   console.log("3. Initializing Firebase...");
